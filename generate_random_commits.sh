6b6a97765e53f3b737e4ae641de9f7901be36c3b
mantas.petrikas <mantas.petrikas@cujo.com>
2023-07-03 10:06:22 +0300

    updated script

--- generate_random_commits.sh
+++ generate_random_commits.sh
@@ -2,11 +2,12 @@
 
 
 # Loop to generate 100 random commits
-for ((i=1; i<=100; i++))
+for ((i=1; i<=1000; i++))
 do
   # Generate a random commit message
-  commit_message=$(curl -s 'https://api.whatdoestrumpthink.com/api/v1/quotes/random' | jq .message)
-
+  # commit_message=$(curl -s 'https://api.whatdoestrumpthink.com/api/v1/quotes/random' | jq .message)
+  random_line=$(gshuf -n1 shakespeare.txt)
+  commit_message="Message ${random_line}"
   # Generate a random file name
   file_name=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 
@@ -20,9 +21,7 @@ do
   git add "files/$file_name.txt"
 
   # Make the commit using the random date and message
-  # echo "$commit_message"
-  git commit -m "$commit_message"
-
+  git commit -m "$commit_message" -q
   # Remove the file
 
 done
